af5570fcae7af0765a1bc61cf68258222e9fa762
lomic <ondrej.lomic@ysoft.com>
2019-12-07 11:49:45 +0100

    empty wolf added

---
+++ src/empty.cpp
@@ -0,0 +1,16 @@
+#include <wolf.h>
+
+int main(int argc, char *argv[]) {
+  using namespace wolf;
+  using std::string;
+
+  pipeline p(argc, argv);
+
+  p.register_plugin(
+      make<tcp::input>(9556),
+      make<tcp::output>("localhost", "9070")
+  );
+  p.run();
+
+  return 0;
+}
